cbbbd722d4117ae97d7393bc2f27377201d8a09a
shreekhand.wankhede <shreekhand.wankhede@gmail.com>
2023-02-20 21:58:12 +0530

    Swagger API UI Documentation initial commit.

---
+++ src/main/java/com/example/springbootswaggeruiexample/SpringBootSwaggerUiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootswaggeruiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwaggerUiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwaggerUiExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootswaggeruiexample/SpringBootSwaggerUiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootswaggeruiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSwaggerUiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
